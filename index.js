c39e4e0422f2263c80bcb7c29bbbf3bca70a1f1d
suhye0n <contact.suhyeon@gmail.com>
2022-12-05 22:15:47 +0900

    Create index.js

--- index.js
+++ index.js
@@ -6,4 +6,16 @@ const app = express();
 app.use(bodyParser.json());
 app.post('/switchLed', function(req,res) {
     const statusExists = rq && req.body && req.body.status != null;
+    const status = statusExists ? req.body.status.toString() == "true":false;
+
+    gpiop.setup(8,gpiop.DIR_OUT).then(()=>{
+        return gpiop.write(8, status);
+    }).catch((err)=>{
+        console.log(`error:${err.toString}`)
+    });
+
+    res.send('led was switched');
 });
+
+app.listen(3000);
+console.log('server 3000');
