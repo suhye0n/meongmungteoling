622e137656ae8dbe6f05880e298b02d3d251ca13
suhye0n <contact.suhyeon@gmail.com>
2022-12-06 14:45:56 +0900

    서버 작성 완료

--- Raspberry/server.c
+++ Raspberry/server.c
@@ -13,6 +13,9 @@
 int main(int argc, char argv[]) {
     int serv;
     int conn;
+    
+    struct sockaddr_in serv_addr;
+    struct sockaddr_in conn_addr;
 
     int addlen, datalen;
 
@@ -37,10 +40,10 @@ int main(int argc, char argv[]) {
         exit(0);
     }
 
-    memset(&serv, 0, sizeof(serv_addr));
-    serv.sin_family = AF_INET;
-    serv.sin_addr.s_addr = htonl(INADDR_ANY);
-    serv.sin_port = htons(nport);
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(nport);
 
     if (bind(serv, (struct sockaddr *)&serv, sizeof(serv)) == -1) {
         perror("bind 에러 \n");
@@ -51,4 +54,43 @@ int main(int argc, char argv[]) {
         perror("클라이언트 접속 에러!");
         exit(0);
     }
+
+    addrlen = sizeof(conn);
+    conn = accept(serv, (struct sockaddr *)&conn_addr, &addrlen);
+    if (conn == -1) {
+        perror("accept 에러! \n");
+        exit(0);
+    }
+
+    if ((pid = fork()) == -1) {
+        close(conn);
+        perror("fork 에러");
+    } else if (pid == 0) {
+        while(1) {
+            fgets(buf, sizeof(buf), stdin);
+            nbytes = strlen(buf);
+            write(conn, buf, MAX);
+            if (stdncmp(buf, "exit", 4) == 0) {
+                puts("프로그램 종료 \n");
+                exit(0);
+            }
+        }
+    } else if (pid > 0) {
+        while(1) {
+            if ((nbytes = read(conn, buf, MAX)) < 0) {
+                perror("read 에러");
+                exit(0);
+            }
+
+            print("%s \n", buf);
+
+            if (strncmp(buf, "exit", 4) == 0) {
+                exit(0);
+            }
+        }
+    }
+
+    close(conn);
+    close(serv);
+    return 0;
 }
