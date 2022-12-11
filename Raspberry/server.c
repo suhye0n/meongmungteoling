5b0a40eed5ece54624a57323e336c8fd6935ce80
JeonYezz <97239535+JeonYezz@users.noreply.github.com>
2022-12-11 23:18:47 +0900

    Update server.c

--- Raspberry/server.c
+++ Raspberry/server.c
@@ -3,91 +3,186 @@
 #include <unistd.h>
 #include <wiringPiI2C.h>
 #include <stdio.h>
-#include <stdlib.h> //atoi를 사용하려면 있어야함
-#include <string.h> // memset 등
-#include <unistd.h> //sockaddr_in, read, write 등
-#include <arpa/inet.h>  //htnol, htons, INADDR_ANY, sockaddr_in 등
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 #include <sys/socket.h>
+#include <pthread.h>
 
 #define SLAVE_ADDR_01 0x48
+#define BUZZ 23
+#define LED 18
+
 static const char* I2C_DEV = "/dev/i2c-1";
 
-void error_handling(char * message);
+int GPIO_control_setup(void);              // GPIO setup 함수
+void *thread_led(void *arg);               // LED 제어 함수     (클라이언트 -> 서버)
+void turn_on_buzzer(int buzz_switch);      // BUZZER 제어 함수  (서버)
+void *thread_temp(void *arg);              // 온도 함수         (서버 -> 클라이언트)
+void error_handling(char *message);        // 에러 발생 유무 확인 함수
 
-int main(int argc, char* argv[]) {
-    int i2c_fd;
+// 공유변수
+int i2c_fd;
+int serv_sock;
+int clnt_sock;
+pthread_mutex_t mutex;
 
-    int preVal = 0;
-    int curVal = 0;
-    int adcChannel = 1;
+// main
+int main(void) {
+    pthread_t p_thread1, p_thread2;  // thread 2개 생성
+    pthread_mutex_init(&mutex, NULL);           // 뮤텍스 초기화
 
-    int val = 0;                // DAC 출력 값을 저장 
-    float outVoltage = 0.0;     // DAC에 출력되는 값을 전압으로 변환한 값을 저장
+    if (GPIO_control_setup()==-1) {
+        return -1;
+    }
 
-    //wiringPi 셋업
+    // thread 생성
+    pthread_create(&p_thread1, NULL, thread_led, NULL);
+    pthread_create(&p_thread2, NULL, thread_temp, NULL);
+
+    // thread 종료 대기
+    pthread_join(p_thread1, NULL);
+    pthread_join(p_thread2, NULL);
+
+    // 소켓들 닫기
+    close(clnt_sock);
+    close(serv_sock);
+
+    // 뮤텍스 제거
+    pthread_mutex_destroy(&mutex);
+    return 0;
+}
+
+// GPIO setup
+int GPIO_control_setup(void)
+{
     if(wiringPiSetupGpio() < 0)
     {
         printf("wiringPiSetup() is failed\n");
         return 1;
     }
-    //i2c연결 시도
+    
     if((i2c_fd = wiringPiI2CSetupInterface(I2C_DEV, SLAVE_ADDR_01)) < 0)
     {
         printf("wiringPiI2CSetup Failed: \n");
         return 1;
     }
     printf("I2C start....\n");
-
-    int serv_sock;
-    int clnt_sock;
    
-    //sockaddr_in은 소켓 주소의 틀을 형셩해주는 구조체로 AF_INET일 경우 사용
     struct sockaddr_in serv_addr;
-    struct sockaddr_in clnt_addr; //accept함수에서 사용됨.
+    struct sockaddr_in clnt_addr;
     socklen_t clnt_addr_size;
    
-    //TCP연결지향형이고 ipv4 도메인을 위한 소켓을 생성
     serv_sock=socket(PF_INET, SOCK_STREAM, 0); 
     if(serv_sock == -1)
          error_handling("socket error");
     
-    //주소를 초기화한 후 IP주소와 포트 지정
     memset(&serv_addr, 0, sizeof(serv_addr)); 
-    serv_addr.sin_family=AF_INET;                //타입: ipv4
-    serv_addr.sin_addr.s_addr=htonl(INADDR_ANY); //ip주소
-    serv_addr.sin_port=htons(atoi(argv[1]));     //port
+    serv_addr.sin_family=AF_INET;
+    serv_addr.sin_addr.s_addr=htonl(INADDR_ANY);
+    serv_addr.sin_port=htons(8080);
     
-    //소켓과 서버 주소를 바인딩
     if(bind(serv_sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr))==-1)
     	error_handling("bind error");
     
-    //연결 대기열 5개 생성 
     if(listen(serv_sock, 5)==-1)
     	error_handling("listen error");
     
-    //클라이언트로부터 요청이 오면 연결 수락
     clnt_addr_size = sizeof(clnt_addr);
     clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
     if(clnt_sock==-1)
         error_handling("accept error");
 
     // 소켓 연결되면 실행
-    wiringPiI2CWrite(i2c_fd, 0x00 | adcChannel);
+    printf("!!!! 소켓 연결됐당 !!!! \n");
+}
 
-    preVal = wiringPiI2CRead(i2c_fd);   // 이전 활성화 값
-    curVal = wiringPiI2CRead(i2c_fd);   // 현재 ADC 값
-    printf("!!!! 소켓 연결됐당 !!!!");
-    
-    printf("Previous value = %d, ", preVal);
-    printf("Current value = %d, ", curVal);
+void turn_on_buzzer(int buzz_switch)
+{
+    pinMode(BUZZ, OUTPUT);
 
-    delay(500);
-    
-    //소켓들 닫기
-    close(clnt_sock);
-    close(serv_sock);
+    if(buzz_switch == 1)
+    {
+        digitalWrite(BUZZ, HIGH);
+        printf("강아지의 체온에 이상이 있습니다\n");
+    }
+    else if(buzz_switch == 0)
+    {
+        digitalWrite(BUZZ,LOW);
+    }
+}
 
+void *thread_led(void *arg)
+{
+    int preVal = 0;         // 이전 밝기
+    int curVal = 0;         // 현재 밝기
+    int led_adcChannel = 2;
+    int duty;
+
+    while(1)
+    {
+        pthread_mutex_lock(&mutex);
+        wiringPiI2CWrite(i2c_fd, 0x00 | led_adcChannel);
+
+        preVal = wiringPiI2CRead(i2c_fd);
+        curVal = wiringPiI2CRead(i2c_fd);
+
+        duty = curVal/100*1024;
+        pinMode(LED, PWM_OUTPUT);
+
+        pwmWrite(LED, duty);
+
+        pthread_mutex_unlock(&mutex);
+        delay(500);
+    }
     return 0;
+}
+
+void *thread_temp(void *arg)
+{
+    int preVal = 0;         // 이전 온도
+    int curVal = 0;         // 현재 온도
+    int temp_adcChannel = 1;
+
+    int val = 0;
+    float outVoltage = 0.0;
+
+    int buzz_switch = 0;
+
+    while(1) 
+    {
+        pthread_mutex_lock(&mutex);
+        wiringPiI2CWrite(i2c_fd, 0x00 | temp_adcChannel);
+        preVal = wiringPiI2CRead(i2c_fd);
+        curVal = wiringPiI2CRead(i2c_fd);
+
+        int test = 38;
+        // char lowbyte = (curVal & 255);
+        // char highbyte = ((curVal >> 8) & 255);
+        char lowbyte = (test & 255);
+        char highbyte = ((test >> 8) & 255);
+        char sd = (highbyte << 8) + lowbyte;
+
+        //printf("이전: %d, ", preVal);
+        //printf("현재: %d \n", curVal);
+        printf("테스트: %d \n", sd);
+
+        send(clnt_sock, (char*)&sd, sizeof(sd), 0);
+
+        if(test <= 37 || test >= 39)
+        {
+            buzz_switch = 1;
+            turn_on_buzzer(buzz_switch);
+        }
+        else
+        {
+            buzz_switch = 0;
+            turn_on_buzzer(buzz_switch);
+        }
+        pthread_mutex_unlock(&mutex);
+        delay(500);
+    }
 }
 
 void error_handling(char *message)
